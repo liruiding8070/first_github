cae6fa61a4f081daae98df8b2bd18fd5b7ca62ac
liruiding <82852026+liruiding8070@users.noreply.github.com>
2022-11-22 11:08:14 +0800

    http post方法实现

--- src/HTTP/HTTP_s.py
+++ src/HTTP/HTTP_s.py
@@ -14,8 +14,12 @@ Content-Type: text/html
 <html>
 <head></head>
 <body>
-    <h1>This is a picture</h1><br>
+    <h1>This is a picture</h1>
     <img src = "img.jpg" width="600"/>
+    <form action = "/" name="input" method="post">
+        First name: <input type = "text" name="FirstName"><br>
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
 '''
@@ -34,10 +38,18 @@ while True:
     print("request: ", request)
     method = request.split(' ')[0]
     src = request.split(' ')[1]
+    print(method)
     if method == "GET":
         if src == "/img.jpg":
             content = pic_content
         else:
             content = text_content
         conn.sendall(content)
+    if method == "POST":
+        form = request.split('\r\n')
+        idx = form.index('')
+        entry = form[idx:]
+        value = entry[-1].split('=')[-1].encode()
+        print(value)
+        conn.sendall(text_content + b'\n<p>' + value + b'</p>')
     conn.close()
